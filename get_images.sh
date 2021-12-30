cb6e302819ca7302227097e2d5c132bdaee6b33b
nrocketmann <nameerhirschkind@gmail.com>
2021-12-29 22:00:49 -0800

    bashfile

---
+++ get_images.sh
@@ -0,0 +1,5 @@
+wget --load-cookies cookies.txt https://www.kaggle.com/mcparadip/anime-faces-waifu2x/download
+unzip download
+mv download images
+mkdir images/subimages
+mv images/*.png images/subimages
